9608da611462d8ab90b2fc7379e404a6132209d2
Mateus Pereira <118226023+mateuscromado@users.noreply.github.com>
2024-10-21 19:31:59 -0300

    Assunto finalizado, agora é só fazer a lógica em JS

---
+++ script.js
@@ -0,0 +1,10 @@
+var questoes = [
+    {
+        
+        titulo: "Qual bioma brasileiro é conhecido por suas savanas e cerrado?",
+        op: ["amazônia","Mata Atlãntica","Pantanal","Caatinga","Cerrado"],
+        correta: 5,
+        selecionada: false
+
+    }
+]
